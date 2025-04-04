34ed07857347da97cc5d3dd4f71bbc5179a50869
twebber03 <twebb21@uic.edu>
2025-04-04 15:18:07 -0500

    unused removal

--- backend/playground/admin.py
+++ backend/playground/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+# How the admin interface for this app is going to look like
 
 # Register your models here.
+
---
+++ backend/playground/serializers.py
@@ -0,0 +1,2 @@
+# Kind of like adapter pattern in android
+# Transforms model into json data that we can access in our api
